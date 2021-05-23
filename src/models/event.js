a51c6820031ed1fb7aae430f28d9166557f6e959
ana-vela <ana@anavela.net>
2021-05-23 01:01:12 -0600

    schema validation

--- src/models/event.js
+++ src/models/event.js
@@ -3,9 +3,20 @@ const mongoose = require("mongoose");
 
 // create schema
 const eventsSchema = new mongoose.Schema({
-  title: String,
-  cost: Number,
-  category: String,
+  title: {
+    type: String,
+    required: true,
+  },
+
+  cost: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+    enum: ["party", "meeting", "other"],
+  },
 });
 
 const Event = mongoose.model("Event", eventsSchema);
