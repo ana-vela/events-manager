15181dad092985bf030c331d78258050ebaee9e4
ana-vela <ana@anavela.net>
2021-05-22 22:19:24 -0600

    Post request to add new event and get request to fetch all events

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const port = 4000;
 const mongoose = require("mongoose");
 const connectionString = "mongodb://localhost:27017/events-manager";
 
+app.use(express.json());
+
 mongoose.connect(
   connectionString,
   {
@@ -31,19 +33,44 @@ const eventsSchema = new mongoose.Schema({
 
 const Event = mongoose.model("Event", eventsSchema);
 
-Event.create(
-  {
-    title: "Bob's birthday",
-    cost: 100,
-    category: "celebration",
-  },
-  (err, event) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(event);
+// POST request to /events to create a new event
+app.post("/events", function (req, res) {
+  //retrieve new event details from req.body
+//   const event = req.body.event;
+  Event.create(
+    {
+      title: event.title,
+      cost: event.cost,
+      category: event.category,
+    },
+    (err, newEvent) => {
+      if (err) {
+        return res.status(500).json({ message: err })
+      } else {
+        return res.status(200).json({ message: "new event created", newEvent });
+      }
     }
-  }
-);
+  );
+  console.log({ event });
+  //create a new event and save to db
+  //send response to client
+});
+
+// GET request to /events to fetch all events
+app.get("/events", (req, res) => {
+  // fetch all events
+  Event.find({}, (err, events) => {
+    if (err) {
+      return res.status(500).json({ message: err });
+    } else {
+      return res.status(200).json({ events });
+    }
+  });
+
+  // send response to client
+});
+// GET request to /events/:id to fetch single event
+// PUT request to /events/:id to update a single event
+// DELETE REQUEST to /books/:id to delete event
 
 app.listen(port, () => console.log(`app listening on port ${port}`));
